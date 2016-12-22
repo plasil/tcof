956e99c83da8c7a0ba56519d92408c5d61758968
Jiri Vinarek <vinarek@d3s.mff.cuni.cz>
2016-12-22 11:29:47 +0100

    moved enumerate Kind to object SearchingMessage

--- src/main/scala/rcrs/searching/SearchingMessage.scala
+++ src/main/scala/rcrs/searching/SearchingMessage.scala
@@ -4,36 +4,100 @@ import rescuecore2.misc.EncodingTools
 import rescuecore2.misc.geometry.spatialindex.RectangleRegion
 import rescuecore2.worldmodel.EntityID
 
-object Kind extends Enumeration {
-  type Kind = Value
+object SearchingMessage {
 
-  /**
-    * Confirming to agent - content = EntityID of agent to which the confirmation belongs
-    */
-  val ACK = Value
+  object Kind extends Enumeration {
+    type Kind = Value
 
-  /**
-    * Announcing myself - content = empty
-    */
-  val ME = Value
+    /**
+      * Confirming to agent - content = EntityID of agent to which the confirmation belongs
+      */
+    val ACK = Value
 
-  /**
-    * Info about a building on fire - content = EntityID of the building
-    */
-  val ONFIRE = Value
+    /**
+      * Announcing myself - content = empty
+      */
+    val ME = Value
 
-  /**
-    * Message to agent to explore a given area - content =  RectangleRegion
-    */
-  val EXPL = Value
+    /**
+      * Info about a building on fire - content = EntityID of the building
+      */
+    val ONFIRE = Value
 
-  /**
-    * Message to agent to extinguish a fire - content = EntityID of the building
-    */
-  val EXTI = Value
+    /**
+      * Message to agent to explore a given area - content =  RectangleRegion
+      */
+    val EXPL = Value
+
+    /**
+      * Message to agent to extinguish a fire - content = EntityID of the building
+      */
+    val EXTI = Value
+  }
+
+  import Kind._
+
+  private def create(bytes: Array[Byte]): SearchingMessage = {
+    val ln = bytes(0)
+    val s = new String(bytes, 1, ln)
+    val localKind = Kind.withName(s)
+    var localContent: AnyRef = null
+
+    localKind match {
+      case ACK | ONFIRE | EXTI =>
+        localContent = new EntityID(SearchingMessage.byteArrayToInt(bytes, 1 + ln))
+      case EXPL =>
+        localContent = new RectangleRegion(
+          EncodingTools.readDouble(bytes, 1 + ln),
+          EncodingTools.readDouble(bytes, 9 + ln),
+          EncodingTools.readDouble(bytes, 17 + ln),
+          EncodingTools.readDouble(bytes, 25 + ln)
+        )
+      case _ =>
+        localContent = new String(bytes, 1 + ln, bytes.length - ln - 1);
+    }
+
+    return new SearchingMessage(localKind, localContent)
+  }
+
+  def fromBytes(bytes: Array[Byte]): SearchingMessage = {
+    return create(bytes)
+  }
+
+  def newME: SearchingMessage = {
+    return new SearchingMessage(ME)
+  }
+
+  def newACK(to: EntityID): SearchingMessage = {
+    return new SearchingMessage(ACK, to)
+  }
+
+  def newONFIRE(building: EntityID): SearchingMessage = {
+    return new SearchingMessage(ONFIRE, building)
+  }
+
+  def newEXPL(r: RectangleRegion): SearchingMessage = {
+    return new SearchingMessage(EXPL, r)
+  }
+
+  def intToByteArray(a: Int): Array[Byte] = {
+    return Array[Byte](
+      ((a >> 24) & 0xFF).toByte,
+      ((a >> 16) & 0xFF).toByte,
+      ((a >> 8) & 0xFF).toByte,
+      (a & 0xFF).toByte
+    )
+  }
+
+  def byteArrayToInt(b: Array[Byte], startIndex: Int): Int = {
+    (b(startIndex + 3) & 0xFF) |
+      ((b(startIndex + 2) & 0xFF) << 8) |
+      ((b(startIndex + 1) & 0xFF) << 16) |
+      ((b(startIndex + 0) & 0xFF) << 24)
+  }
 }
 
-import Kind._
+import SearchingMessage.Kind._
 
 class SearchingMessage private (_kind: Kind, _content: AnyRef) {
 
@@ -69,66 +133,4 @@ class SearchingMessage private (_kind: Kind, _content: AnyRef) {
     return ret
   }
 
-}
-
-object SearchingMessage {
-
-  private def create(bytes: Array[Byte]): SearchingMessage = {
-    val ln = bytes(0)
-    val s = new String(bytes, 1, ln)
-    val localKind = Kind.withName(s)
-    var localContent: AnyRef = null
-
-    localKind match {
-      case ACK | ONFIRE | EXTI =>
-        localContent = new EntityID(SearchingMessage.byteArrayToInt(bytes, 1 + ln))
-      case EXPL =>
-        localContent = new RectangleRegion(
-          EncodingTools.readDouble(bytes, 1 + ln),
-          EncodingTools.readDouble(bytes, 9 + ln),
-          EncodingTools.readDouble(bytes, 17 + ln),
-          EncodingTools.readDouble(bytes, 25 + ln)
-        )
-      case _ =>
-        localContent = new String(bytes, 1 + ln, bytes.length - ln - 1);
-    }
-
-    return new SearchingMessage(localKind, localContent)
-  }
-
-  def fromBytes(bytes: Array[Byte]): SearchingMessage = {
-    return create(bytes)
-  }
-
-  def newME: SearchingMessage = {
-    return new SearchingMessage(Kind.ME)
-  }
-
-  def newACK(to: EntityID): SearchingMessage = {
-    return new SearchingMessage(Kind.ACK, to)
-  }
-
-  def newONFIRE(building: EntityID): SearchingMessage = {
-    return new SearchingMessage(Kind.ONFIRE, building)
-  }
-
-  def newEXPL(r: RectangleRegion): SearchingMessage = {
-    return new SearchingMessage(Kind.EXPL, r)
-  }
-
-  def intToByteArray(a: Int): Array[Byte] = {
-    return Array[Byte](
-      ((a >> 24) & 0xFF).toByte,
-      ((a >> 16) & 0xFF).toByte,
-      ((a >> 8) & 0xFF).toByte,
-      (a & 0xFF).toByte
-    )
-  }
-
-  def byteArrayToInt(b: Array[Byte], startIndex: Int): Int = {
-    (b(startIndex + 3) & 0xFF) |
-    ((b(startIndex + 2) & 0xFF) << 8) |
-    ((b(startIndex + 1) & 0xFF) << 16) |
-    ((b(startIndex + 0) & 0xFF) << 24)
-  }
 }
